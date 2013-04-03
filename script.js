0b54a9f02bbbea593fb4a5ff2d8bbbf414d80c71
Johann <git@johann-hofmann.com>
2013-04-03 23:56:01 +0200

    add script

--- script.js
+++ script.js
@@ -1,62 +1,43 @@
 (function () {
+    var script = document.createElement('script');
+    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
-    // the minimum version of jQuery we want
-    var v = "1.8.2";
+    var divTag = document.createElement("div");
+    divTag.id = "typreviewDiv";
+    divTag.setAttribute("align", "center");
+    divTag.style.margin = "0px auto";
+    divTag.style.width = "100%";
+    divTag.style.height = "150px";
+    divTag.style.backgroundColor = "green";
+    divTag.style.left = 0;
+    divTag.style.bottom = 0;
+    divTag.style.position = "fixed";
+    divTag.style.zIndex = "200";
 
-    // check prior inclusion and version
-    if(window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-        script.onload = script.onreadystatechange = function () {
-            if(!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                start();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }
-    else {
-        start();
-    }
+    divTag.innerHTML = "Test";
 
-    function start() {
-            var divTag, WebFontConfig, wf, s, button;
-            divTag = document.createElement("div");
-            divTag.id = "typreviewDiv";
-            divTag.setAttribute("align", "center");
-            divTag.style.width = "150px";
-            divTag.style.height = "150px";
-            divTag.style.backgroundColor = "lightgrey";
-            divTag.style.left = 0;
-            divTag.style.bottom = 0;
-            divTag.style.position = "fixed";
-            divTag.style.zIndex = "999";
+    document.body.insertBefore(divTag, document.body.firstChild);
 
-            divTag.innerHTML = "Test";
+    var button = document.createElement("button");
 
-            document.body.insertBefore(divTag, document.body.firstChild);
+    button.innerHTML = "Click Me!";
 
-            button = document.createElement("button");
+    document.getElementById("typreviewDiv").appendChild(button);
 
-            button.innerHTML = "Click Me!";
+    WebFontConfig = {
+        google : { families : [ 'Droid Sans', 'Droid Serif' ] }
+    };
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
 
-            document.getElementById("typreviewDiv").appendChild(button);
-
-            WebFontConfig = {
-                google : { families : [ 'Droid Sans', 'Droid Serif' ] }
-            };
-            wf = document.createElement('script');
-            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-            wf.type = 'text/javascript';
-            wf.async = 'true';
-            s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(wf, s);
-
-            jQuery("button").click(function () {
-                jQuery("body *").css("font-family", "Droid Sans");
-            });
-    }
-
+    jQuery("button").click(function () {
+        jQuery("body *").css("font-family", "Droid Sans");
+    });
 }());
