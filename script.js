753087514eca471e10dfd5a3ba1a21f27884ae09
Johann <git@johann-hofmann.com>
2013-04-04 00:11:52 +0200

    add script

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
 (function () {
-    // the minimum version of jQuery we want
-    var v = "1.8.3";
+    var v, divTag, button, wf, WebFontConfig, s;
 
-    // check prior inclusion and version
+    v = "1.8.3";
+
     if(window.jQuery !== undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
+        var done, script;
+        done = false;
+        script = document.createElement("script");
         script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
         script.onload = script.onreadystatechange = function () {
             if(!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
                 done = true;
-                initMyBookmarklet();
             }
         };
         document.getElementsByTagName("head")[0].appendChild(script);
     }
 
-    var divTag = document.createElement("div");
-    divTag.id = "typreviewDiv";
+    divTag = document.createElement("div");
+    divTag.id = "fontmarkletDiv";
     divTag.setAttribute("align", "center");
-    divTag.style.margin = "0px auto";
-    divTag.style.width = "100%";
+    divTag.style.width = "150px";
     divTag.style.height = "150px";
-    divTag.style.backgroundColor = "green";
+    divTag.style.backgroundColor = "#cccccc";
     divTag.style.left = 0;
     divTag.style.bottom = 0;
     divTag.style.position = "fixed";
-    divTag.style.zIndex = "200";
+    divTag.style.zIndex = "999";
 
     divTag.innerHTML = "Test";
 
     document.body.insertBefore(divTag, document.body.firstChild);
 
-    var button = document.createElement("button");
+    button = document.createElement("button");
 
     button.innerHTML = "Click Me!";
 
-    document.getElementById("typreviewDiv").appendChild(button);
+    document.getElementById("fontmarkletDiv").appendChild(button);
 
     WebFontConfig = {
         google : { families : [ 'Droid Sans', 'Droid Serif' ] }
     };
-    var wf = document.createElement('script');
+    wf = document.createElement('script');
     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
         '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
     wf.type = 'text/javascript';
     wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
+    s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
 
     jQuery("button").click(function () {
