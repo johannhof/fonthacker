881c41a6ced29fef42550613b22e523119f080d2
Johann <git@johann-hofmann.com>
2013-04-10 22:23:53 +0200

    font weight

--- script.js
+++ script.js
@@ -2,25 +2,55 @@
     var v = "1.8.3", script, done = false;
 
     function loadBookmarklet() {
-        var $ = window.jQuery, applyButton, addButton, mainContainer, changeContainer, wf, s, select, input, items, subsetSelect;
+        var $ = window.jQuery, applyButton, addButton, mainContainer,
+            changeContainer, wf, s, select, input, items, subsetSelect,variantSelect;
+
+        function parseVariant(variant){
+            switch (variant){
+                case "regular":
+                    return "normal";
+                case "italic":
+                    return 400;
+                default:
+                    var test = parseInt(variant,10)
+                    return test;
+            }
+        }
+
+        function parseStyle(variant){
+            if(variant.indexOf("regular") !== -1){
+                return "normal";
+            }
+            if(variant.indexOf("italic") !== -1){
+                return "italic";
+            }
+            return "normal";
+        }
 
         /**
          * Loads the specified font from Google Web Fonts
          *
          * @param {string} name the name of the font
          * @param {string} selector the jquery selector pointing to the item that should receive the new fontface
-         * @param {number} [size]
+         * @param {number,optional} [size] the size of the font
+         * @param {string,optional} [variant]
+         * @param {string,optional} [subset] the subset of the font
          */
-        function loadFont(name, selector, size) {
+        function loadFont(name, selector, size, variant, subset) {
+            var options = ":" + (variant || "") + ":" + (subset || "");
             //noinspection JSUnresolvedVariable,JSHint,JSLint
             WebFont.load({
                 google : {
-                    families : [ name ]
+                    families : [ name + options ]
                 },
                 active : function () {
                     $(selector).css("font-family", name);
                     if(size) {
                         $(selector).css("font-size", size + "px");
+                    }
+                    if(variant){
+                        $(selector).css("font-weight", parseVariant(variant));
+                        $(selector).css("font-style",parseStyle(variant));
                     }
                 }
             });
@@ -37,6 +67,17 @@
             return newSubsetSelect;
         }
 
+        function createVariants(font) {
+            var i, numberOfVariants = font.variants.length, newVariantSelect = document.createElement("select"), option;
+            newVariantSelect.className = "variant";
+            for(i = 0; i < numberOfVariants; i++) {
+                option = document.createElement("option");
+                option.innerHTML = font.variants[i];
+                newVariantSelect.appendChild(option);
+            }
+            return newVariantSelect;
+        }
+
         function createFontChanger() {
             var singleInputContainer = document.createElement("div"),
                 selectorInput = document.createElement("input"),
@@ -49,8 +90,10 @@
             singleInputContainer.appendChild(selectClone);
             $(selectClone).change(function () {
                 $(this).parent().children(".subset").replaceWith(createSubsets(items[$(this).val()]));
+                $(this).parent().children(".variant").replaceWith(createVariants(items[$(this).val()]));
             });
             singleInputContainer.appendChild(subsetSelect.cloneNode(true));
+            singleInputContainer.appendChild(variantSelect.cloneNode(true));
             changeContainer.appendChild(singleInputContainer);
         }
 
@@ -109,6 +152,8 @@
         select.className = "selectFont";
         subsetSelect = document.createElement("select");
         subsetSelect.className = "subset";
+        variantSelect = document.createElement("select");
+        variantSelect.className = "variant";
 
         $.getJSON("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBRh3XwaTyAoCjBuAFQ6syYtRjRRdeJb4o&callback=?", function (data) {
             items = data.items;
@@ -128,7 +173,7 @@
         $(applyButton).click(function () {
             var i, nodes = changeContainer.childNodes, max = nodes.length;
             for(i = 0; i < max; i++) {
-                loadFont(items[$(nodes[i]).children(".selectFont").val()].family, $(nodes[i]).children(".selector").val(), $(nodes[i]).children(".fontSize").val());
+                loadFont(items[$(nodes[i]).children(".selectFont").val()].family, $(nodes[i]).children(".selector").val(), $(nodes[i]).children(".fontSize").val(), $(nodes[i]).children(".variant").val(), $(nodes[i]).children(".subset").val());
             }
         });
     }
