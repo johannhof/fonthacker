997c2f708135268c8cd7a2e6dd8a358203752f29
johann <git@johann-hofmann.com>
2014-08-11 18:03:36 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var bookmarklet = document.getElementById("bookmarklet");
+var overlay = document.getElementById("overlay");
+
+bookmarklet.addEventListener('mouseover', function () {
+  overlay.classList.add('visible');
+});
+
+bookmarklet.addEventListener('mouseout', function () {
+  overlay.classList.remove('visible');
+});
