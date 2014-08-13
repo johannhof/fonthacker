16d7868b39f1f0a219ea6c9e21c914a63fba1b44
johann <git@johann-hofmann.com>
2014-08-13 08:53:34 +0200

    load fontmarklet

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var bookmarklet = document.getElementById("bookmarklet");
 var overlay = document.getElementById("overlay");
 
-bookmarklet.addEventListener('mouseover', function () {
+bookmarklet.addEventListener('mouseover', function() {
   overlay.classList.add('visible');
 });
 
-bookmarklet.addEventListener('mouseout', function () {
+bookmarklet.addEventListener('mouseout', function() {
   overlay.classList.remove('visible');
 });
 
-//loadFontmarklet(document.getElementById("fm-container-1"));
+setTimeout(function() {
+  loadFontmarklet(document.getElementById("fm-container-1"));
+}, 3000);
