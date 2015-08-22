5f0d8ab671ae05c89e49c97979b66a500d3844b0
Johann <git@johann-hofmann.com>
2015-08-22 20:18:41 +0200

    Fix bookmarklet creation

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,60 +1,12 @@
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
-script.src = 'https://joo.crater.uberspace.de/fontmarklet/fontmarklet.min.js?development=' + Math.random();
+if (process.env.NODE_ENV === 'production'){
+  script.src = 'https://joo.crater.uberspace.de/fontmarklet/fontmarklet.min.js';
+}else{
+  script.src = 'http://localhost:8080/fontmarklet.js?development=' + Math.random();
+}
 head.appendChild(script);
 
-var style = document.createElement('style');
-style.innerHTML =
-"@-webkit-keyframes fm-loader-rotate {\
-  0% {\
-    transform: rotateZ(0deg);\
-  }\
-  45% {\
-    transform: rotateZ(180deg);\
-  }\
-  55% {\
-    transform: rotateZ(180deg);\
-  }\
-  100% {\
-    transform: rotateZ(360deg);\
-  }\
-}\
-.fm-loader-logo {\
-  top: 50%;\
-  left: 50%;\
-  width: 50px;\
-  height: 50px;\
-  background-color: deepskyblue;\
-  border-radius: 50px;\
-  color: white;\
-  position: fixed;\
-  overflow: hidden;\
-  border: 5px solid deepskyblue;\
-  -webkit-animation-name: fm-loader-rotate;\
-  -webkit-animation-delay: 1s;\
-  -webkit-animation-duration: 2s;\
-  -webkit-animation-iteration-count: infinite;\
-}\
-.fm-loader-logo span {\
-  font-family: Lato;\
-  position: absolute;\
-  left: 5px;\
-  top: -9px;\
-  font-size: 65px;\
-  font-weight: 600;\
-  font-style: italic;\
-}\
-";
-head.appendChild(style);
-
-var loader = document.createElement('div');
-loader.classList.add('fm-loader-logo');
-loader.innerHTML = '<span>F</span>';
-
 script.onload = function () {
-  window.loadFontmarklet(null, function () {
-    loader.style.display = 'none';
-  });
+  window.loadFontmarklet(null);
 };
-
-document.body.appendChild(loader);
